e380fec74403b60dc3dacf7550ae272640ae9e4d
Connor <cgbroadfoot@gmail.com>
2023-01-18 22:10:17 +0000

    snake project functioning. This udpate was across all files (play, input, client)

--- play.js
+++ play.js
@@ -1,10 +1,11 @@
-const { connect } = require("./client");
-const { setupInput } = require("./input");
+const connect = require("./client");
+const setupInput = require("./input");
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+let conn = connect();
+
+setupInput(conn);
 
 
 
