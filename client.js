8fffb8b538f5a356fe1a0965304d4431ed6a94f5
Connor <cgbroadfoot@gmail.com>
2023-01-18 16:52:13 +0000

    added client.js to repo

---
+++ client.js
@@ -0,0 +1,27 @@
+// establishes a connection with the game server
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '10.0.2.15',
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    conn.write("Name: DOG");
+  })  
+
+  // Receive data when connected
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  
+
+  return conn;
+};
+
+module.exports = connect
